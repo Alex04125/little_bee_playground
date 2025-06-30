8e1b2c508940fc7f39f56644fe42ef8a2680e3b8
Alex04125 <102294585+Alex04125@users.noreply.github.com>
2025-06-30 18:49:56 +0300

    Fix elevnty

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
 module.exports = function (eleventyConfig) {
-  // Copy the entire img folder to _site
-  eleventyConfig.addPassthroughCopy("img");
-
   return {
     dir: {
       input: ".",
       includes: "_includes",
       output: "_site",
     },
+    // Process HTML files as templates
+    htmlTemplateEngine: "njk",
   };
 };
